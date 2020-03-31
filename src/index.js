bc091d210e392981de359d52dd8605a3d85a442f
Manuel Schurr <admin@manuelschurr.com>
2020-03-31 12:28:54 +0200

    Added TODOs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// TODO:
+// Display the location for each move in the format (col, row) in the move history list.
+// Bold the currently selected item in the move list.
+// Rewrite Board to use two loops to make the squares instead of hardcoding them.
+// Add a toggle button that lets you sort the moves in either ascending or descending order.
+// When someone wins, highlight the three squares that caused the win.
+// When no one wins, display a message about the result being a draw.
+
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
